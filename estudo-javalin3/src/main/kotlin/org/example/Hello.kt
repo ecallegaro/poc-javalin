70b987459fc76df0419a11dcafacdc34e88cef13
vil_ecallegaro <vil_ecallegaro@uolinc.com>
2021-04-20 14:51:49 -0300

    Projeto estudo kotlin x javalin framework

---
+++ estudo-javalin3/src/main/kotlin/org/example/Hello.kt
@@ -0,0 +1,52 @@
+package org.example
+
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.post
+import io.javalin.apibuilder.ApiBuilder.delete
+import io.javalin.apibuilder.ApiBuilder.patch
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val userDao = UserDao()
+
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        error(404) { ctx -> ctx.json("not found") }
+    }.start(7000)
+
+    app.routes {
+
+        get("/users") { ctx ->
+            ctx.json(userDao.users)
+        }
+
+        get("/users/:user-id") { ctx ->
+            ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+        }
+
+        get("/users/email/:email") { ctx ->
+            ctx.json(userDao.findByEmail(ctx.pathParam("email"))!!)
+        }
+
+        post("/users") { ctx ->
+            val user = ctx.body<User>()
+            userDao.save(name = user.name, email = user.email)
+            ctx.status(201)
+        }
+
+        patch("/users/:user-id") { ctx ->
+            val user = ctx.body<User>()
+            userDao.update(
+                id = ctx.pathParam("user-id").toInt(),
+                user = user
+            )
+            ctx.status(204)
+        }
+
+        delete("/users/:user-id") { ctx ->
+            userDao.delete(ctx.pathParam("user-id").toInt())
+            ctx.status(204)
+        }
+    }
+}
+
